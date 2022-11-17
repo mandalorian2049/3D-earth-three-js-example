d0c243788c903738d9c79801df4f7fc8fb8c5960
Wei <catsass19@hotmail.com>
2022-11-17 12:19:31 +0800

    current setup

--- app/component/Main/index.tsx
+++ app/component/Main/index.tsx
@@ -1,23 +1,24 @@
-import React, { lazy, Suspense } from 'react';
-const Globe = lazy(() => import(/* webpackChunkName: "Universe" */'@/component/Universe'));
+import React, { lazy, Suspense } from "react";
+const Globe = lazy(
+  () => import(/* webpackChunkName: "Universe" */ "@/component/Universe")
+);
 
-const mainStyle : React.CSSProperties = {
-    display: 'flex',
-    flexDirection: 'column',
-    height: '100vh',
-    width: '100vw',
+const mainStyle: React.CSSProperties = {
+  display: "flex",
+  flexDirection: "column",
+  height: "98vh",
 };
 
 class Main extends React.Component {
-    public render() {
-        return (
-          <div style={mainStyle}>
-              <Suspense fallback={<div>Loading...</div>}>
-                <Globe />
-              </Suspense>
-          </div>
-        );
-    }
+  public render() {
+    return (
+      <div style={mainStyle}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Globe />
+        </Suspense>
+      </div>
+    );
+  }
 }
 
 const AppMain = <Main />;
--- app/component/Universe/index.tsx
+++ app/component/Universe/index.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 import * as THREE from "three";
 import debounce from "lodash/debounce";
-import OrbitControl from "three-orbit-controls";
+import gsap from "gsap";
+// import OrbitControl from "three-orbit-controls";
 
-const colorOption = [0x666666, 0x888888, 0xaaaaaa];
+enum CAM_MOVING_DIRECTION {
+  IN,
+  OUT,
+}
+
+export const CAMERA_POSITION_Y = {
+  START: 5,
+  ZOOM_IN: 0.1,
+  ZOOM_OUT: 30,
+};
+
+const colorOption = [
+  0x111111, 0x111111, 0x111111, 0x111111, 0x111111, 0x112222, 0x113333,
+  0x114444, 0x335555, 0x88dddd,
+];
 
 let i = 0;
 const getColor = () => {
@@ -19,7 +34,7 @@ const getVectors = (num: number) => {
         Math.random() * 2 - 1,
         Math.random() * 2 - 1,
         Math.random() * 2 - 1
-      )
+      ).normalize()
     );
   }
   return vectors;
@@ -27,15 +42,17 @@ const getVectors = (num: number) => {
 
 const wrapperStyle: React.CSSProperties = {
   flex: 1,
-  padding: "3px",
+  // padding: "3px",
   display: "flex",
   flexDirection: "column",
 };
 const innerStyle: React.CSSProperties = {
   flex: 1,
-  border: "1px solid #444",
   overflow: "hidden",
 };
+
+let finished = false;
+let stopRotate = false;
 
 class Universe extends React.Component {
   private content;
@@ -43,6 +60,8 @@ class Universe extends React.Component {
   private camera;
   private renderer;
   private control;
+
+  private camMovingDirection: CAM_MOVING_DIRECTION = CAM_MOVING_DIRECTION.IN;
 
   private origin = new THREE.Vector3(0, 0, 0);
 
@@ -54,9 +73,62 @@ class Universe extends React.Component {
 
   public componentDidMount() {
     this.init();
-    this.addAxis();
+    // this.addAxis();
     this.renderArrows();
+
+    document.addEventListener(
+      "keydown",
+      (event) => {
+        const { code } = event;
+        if (code === "Space") {
+          stopRotate = true;
+          this.cameraMovingHandler();
+        }
+      },
+      false
+    );
   }
+
+  private cameraMovingHandler() {
+    if (finished) {
+      location.reload();
+      return;
+    }
+
+    const yPosition = {
+      [CAM_MOVING_DIRECTION.IN]: CAMERA_POSITION_Y.ZOOM_IN,
+      [CAM_MOVING_DIRECTION.OUT]: CAMERA_POSITION_Y.ZOOM_OUT,
+    }[this.camMovingDirection];
+
+    const duration = {
+      [CAM_MOVING_DIRECTION.IN]: 2,
+      [CAM_MOVING_DIRECTION.OUT]: 6,
+    }[this.camMovingDirection];
+
+    this.moveCamOnY(yPosition, duration);
+
+    if (this.camMovingDirection === CAM_MOVING_DIRECTION.IN) {
+      this.camMovingDirection = CAM_MOVING_DIRECTION.OUT;
+      return;
+    }
+
+    if (this.camMovingDirection === CAM_MOVING_DIRECTION.OUT) {
+      finished = true;
+      return;
+    }
+  }
+
+  private moveCamOnY(y: number, duration = 1.5) {
+    gsap.to(this.camera.position, { y, duration });
+  }
+
+  private cameraRotation = () => {
+    if (stopRotate) {
+      return;
+    }
+    this.camera.rotation.z += Math.PI / 50000;
+  };
+
   public componentWillUnmount() {
     window.removeEventListener("resize", this.resizeHandler);
   }
@@ -70,15 +142,15 @@ class Universe extends React.Component {
   }
   private init = () => {
     this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(50, null, 0.1, 1000);
-    this.camera.position.set(40, 5, 0);
+    this.camera = new THREE.PerspectiveCamera(50, null, 3, 1000);
+    this.camera.position.set(0, CAMERA_POSITION_Y.START, 0);
     this.camera.lookAt(new THREE.Vector3(0, 0, 0));
     this.renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
-    this.control = new (OrbitControl(THREE))(
-      this.camera,
-      this.renderer.domElement
-    );
-    this.control.zoomSpeed = 0.2;
+    // this.control = new (OrbitControl(THREE))(
+    //   this.camera,
+    //   this.renderer.domElement
+    // );
+    // this.control.zoomSpeed = 0.2;
     this.renderer.render(this.scene, this.camera);
     this.renderer.setSize(this.content.clientWidth, this.content.clientHeight);
     this.content.appendChild(this.renderer.domElement);
@@ -88,36 +160,37 @@ class Universe extends React.Component {
   };
 
   private addAxis = () => {
-    const gridHelper = new THREE.GridHelper(100, 30, 0x444444, 0x333333);
+    const gridHelper = new THREE.GridHelper(50, 30, 0x444444, 0x333333);
     this.scene.add(gridHelper);
   };
 
   private animate = () => {
-    this.control.update();
+    // this.control.update();
+    this.cameraRotation();
     requestAnimationFrame(this.animate);
     this.renderer.render(this.scene, this.camera);
   };
   private setContent = (ref) => (this.content = ref);
 
   private renderArrows = () => {
-    getVectors(3348).forEach((vector) => {
-      this.addArrow(vector, Math.random() + 4, getColor());
+    getVectors(3000).forEach((vector) => {
+      this.addArrow(vector, Math.random() / 4 + 2, getColor());
     });
 
     const sum = new THREE.Vector3(1, 1.5, 1);
     sum.normalize();
-    this.addArrow(sum, 15, new THREE.Color("cyan"), 1);
+    // this.addArrow(sum, 15, new THREE.Color("cyan"), 1);
   };
 
-  private addArrow = (dir, length = 10, color = 0xffff00, headSize = 0.4) => {
-    const arrowHelper = new THREE.ArrowHelper(
+  private addArrow = (dir, length = 10, color = 0xffff00, headSize = 0.2) => {
+    const arrow = new THREE.ArrowHelper(
       dir,
-      this.origin,
+      dir.multiplyScalar((Math.random() + 0.1) * 1.5),
       length,
       color,
       headSize
     );
-    this.scene.add(arrowHelper);
+    this.scene.add(arrow);
   };
 }
 
